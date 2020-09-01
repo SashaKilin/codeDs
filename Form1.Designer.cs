f8eafa0265799acf6ed37d6f1856b849d9c7d9fa
SashaKilin <66529400+SashaKilin@users.noreply.github.com>
2020-09-01 17:49:27 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace just
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label10 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(740, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "×";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.label1.MouseHover += new System.EventHandler(this.label1_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "OS:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(0, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Procces Count:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(12, 405);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Drives:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(0, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 31);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(418, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(242, 25);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Free Disk Space ( C:\\\\ ):";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Controls.Add(this.label9);
+            this.panel4.Location = new System.Drawing.Point(418, 123);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(355, 56);
+            this.panel4.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(0, 12);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 31);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "label9";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(418, 247);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(355, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(419, 198);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(124, 25);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Disk layout:";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(424, 470);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(355, 23);
+            this.progressBar2.TabIndex = 13;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(425, 295);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(242, 25);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Free Disk Space ( D:\\\\ ):";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Controls.Add(this.label12);
+            this.panel5.Location = new System.Drawing.Point(424, 333);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(355, 56);
+            this.panel5.TabIndex = 11;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.ForeColor = System.Drawing.Color.Black;
+            this.label12.Location = new System.Drawing.Point(0, 12);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(101, 31);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "label12";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.label13.ForeColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(425, 426);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(124, 25);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "Disk layout:";
+            // 
+            // label15
+            // 
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label15.ForeColor = System.Drawing.Color.White;
+            this.label15.Location = new System.Drawing.Point(12, 193);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(86, 29);
+            this.label15.TabIndex = 4;
+            this.label15.Text = "Name:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label14.ForeColor = System.Drawing.Color.Black;
+            this.label14.Location = new System.Drawing.Point(0, 14);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(101, 31);
+            this.label14.TabIndex = 1;
+            this.label14.Text = "label14";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(0, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 31);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Location = new System.Drawing.Point(12, 443);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(355, 56);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(12, 123);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(355, 56);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.White;
+            this.panel6.Controls.Add(this.label14);
+            this.panel6.Location = new System.Drawing.Point(12, 225);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(355, 56);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(12, 333);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(355, 56);
+            this.panel2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(0, 533);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(792, 36);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Update Informations";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(792, 569);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace just
+{
+    public partial class Form1 : Form
+    {
+        public long all_place;
+        public long free_place;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Red;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.FromArgb(0, 0, 64);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                label1.BackColor = Color.Red;
+            });
+
+            this.Close();
+        }
+
+        Point lastPoint;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            label3.Text = Convert.ToString(Environment.OSVersion);
+            label7.Text = Convert.ToString(string.Join(", ", Environment.GetLogicalDrives())
+                .TrimEnd(',', ' ')
+                .Replace("\\", string.Empty));
+
+            DriveInfo[] allDrive = DriveInfo.GetDrives();
+
+            foreach (var d in allDrive)
+            {
+                if (d.Name == "C:\\")
+                {
+                    long all_place = d.TotalSize;
+                    long free_place = d.TotalFreeSpace;
+
+                    all_place = all_place / 1000000000;
+                    free_place = free_place / 1000000000;
+
+                    progressBar1.Maximum = Convert.ToInt32(all_place);
+                    progressBar1.Value = Convert.ToInt32(all_place - free_place);
+                    label9.Text = Convert.ToString(free_place + " GB / " + all_place) + " GB";
+                }
+                if (d.Name == "D:\\")
+                {
+                    long all_place = d.TotalSize;
+                    long free_place = d.TotalFreeSpace;
+
+                    all_place = all_place / 1000000000;
+                    free_place = free_place / 1000000000;
+
+                    progressBar2.Maximum = Convert.ToInt32(all_place);
+                    progressBar2.Value = Convert.ToInt32(all_place - free_place);
+                    label12.Text = Convert.ToString(free_place + " GB / " + all_place) + " GB";
+                }
+            }
+
+            int counttt = 0;
+            Process[] allProc = Process.GetProcesses();
+            foreach (var i in allProc)
+            {
+                counttt++;
+            }
+
+            label5.Text = Convert.ToString(counttt);
+
+            label14.Text = Environment.MachineName;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = Convert.ToString(Environment.OSVersion);
+            label7.Text = Convert.ToString(string.Join(", ", Environment.GetLogicalDrives())
+                .TrimEnd(',', ' ')
+                .Replace("\\", string.Empty));
+
+            DriveInfo[] allDrive = DriveInfo.GetDrives();
+
+            foreach (var d in allDrive)
+            {
+                if (d.Name == "C:\\")
+                {
+                    long all_place = d.TotalSize;
+                    long free_place = d.TotalFreeSpace;
+
+                    all_place = all_place / 1000000000;
+                    free_place = free_place / 1000000000;
+
+                    progressBar1.Maximum = Convert.ToInt32(all_place);
+                    progressBar1.Value = Convert.ToInt32(all_place - free_place);
+                    label9.Text = Convert.ToString(free_place + " GB / " + all_place) + " GB";
+                }
+                if (d.Name == "D:\\")
+                {
+                    long all_place = d.TotalSize;
+                    long free_place = d.TotalFreeSpace;
+
+                    all_place = all_place / 1000000000;
+                    free_place = free_place / 1000000000;
+
+                    progressBar2.Maximum = Convert.ToInt32(all_place);
+                    progressBar2.Value = Convert.ToInt32(all_place - free_place);
+                    label12.Text = Convert.ToString(free_place + " GB / " + all_place) + " GB";
+                }
+            }
+
+            int countt = 0;
+            Process[] allProc = Process.GetProcesses();
+            foreach (var i in allProc)
+            {
+                countt++;
+            }
+
+            label5.Text = Convert.ToString(countt);
+
+            label14.Text = Environment.MachineName;
+
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Green;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+        }
+
+        private string end;
+        private int count;
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Process[] allProcces = Process.GetProcesses();
+            foreach (var i in allProcces)
+            {
+                end = end + "    " + i;
+                count++;
+            }
+
+            MessageBox.Show(end + "\n\nВсего " + count + " процессов(а)");
+
+            end = "";
+            count = 0;
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\");
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\");
+        }
+    }
+}
